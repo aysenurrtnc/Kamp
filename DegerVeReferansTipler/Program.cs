7980fb38c4b23c16145c9b903775a7b0ac2c7bc5
aysenurtunc8888 <aysenur.tunc@bahcesehir.edu.tr>
2021-01-17 15:03:57 +0300

    Initial check-in of module Metotlar

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+         
+
+        }
+    }
+}
