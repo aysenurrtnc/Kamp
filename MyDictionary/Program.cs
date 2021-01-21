5ff8cc058e85f28bca8b2e7aa74833ce405a1927
aysenurtunc8888 <59649434+aysenurtunc8888@users.noreply.github.com>
2021-01-21 23:44:38 +0300

    Add files via upload

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MyDictionaryDemo<int, string> cities = new MyDictionaryDemo<int, string>();
+            cities.Add(47, "Mardin");
+            cities.Add(35, "İzmir");
+            cities.Add(06, "Ankara");
+            cities.Add(34, "İstanbul");
+            cities.Add(61, "Trabzon");
+
+
+
+            foreach (var city in cities.Values)
+            {
+                Console.WriteLine(city);
+            }
+
+            foreach (var plates in cities.Key)
+            {
+                Console.WriteLine(plates);
+            }
+
+        }
+    }
+
+  
+}
